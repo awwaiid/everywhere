1f1fdc608269df1173b7349bba97e0c835b80b8c
awwaiid <awwaiid@thelackthereof.org>
2009-09-03 08:18:24 -0400

    Version bump
    darcs-hash:e68231b6da38294221b1dbdc46040d9c6fc2f1c5

--- lib/everywhere.pm
+++ lib/everywhere.pm
@@ -42,7 +42,7 @@ regex. And not use it here. This is experimental :)
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub import {
   my ($class, $module, @items) = @_;
